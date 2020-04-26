191d72f088f054b41b4997be00cb7450d3e6472b
akbog <alex.bogdanowicz@nyu.edu>
2020-04-26 02:47:46 -0400

    Batch Functionality for Database Import

--- Modules/TwitterDatabaseUpdate.py
+++ Modules/TwitterDatabaseUpdate.py
@@ -195,7 +195,7 @@ class TwitterDatabase(object):
                     %(num_favourites)s,
                     %(num_tweets)s,
                     %(located)s
-                ) ON CONFLICT user_id DO NOTHING;
+                ) ON CONFLICT users.user_id DO NOTHING;
             """, all_users)
 
     def add_all_tweets(self):
@@ -218,7 +218,7 @@ class TwitterDatabase(object):
                     %(retweet_count)s,
                     %(favorite_count)s,
                     %(language)s
-                ) ON CONFLICT id DO NOTHING;
+                ) ON CONFLICT tweet.id DO NOTHING;
             """, all_tweets)
 
     def add_file(self, fileid, filekey):
