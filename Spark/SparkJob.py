c482a0beca4d71f6b5e60a2143a6899abb7ceb6e
akbog <alex.bogdanowicz@nyu.edu>
2020-07-10 19:52:54 -0400

    changes to spark

--- Spark/SparkJob.py
+++ Spark/SparkJob.py
@@ -5,6 +5,7 @@ from pyspark.sql.functions import mean as _mean, stddev as _stddev, col, udf
 from textblob import TextBlob
 
 import sparknlp
+from sparknlp import SparkSession
 from sparknlp.base import Finisher, DocumentAssembler
 from sparknlp.annotator import Tokenizer, Normalizer, LemmatizerModel, StopWordsCleaner, ViveknSentimentApproach, SentimentDetector, SentenceDetector, Stemmer, Lemmatizer
 from pyspark.ml import Pipeline
@@ -13,10 +14,9 @@ from pyspark.ml import Pipeline
 if __name__ == "__main__":
     #Launching Spark
     spark = sparknlp.start()
+
     sqlCtx = SQLContext(spark)
     spark.sparkContext.setLogLevel('ERROR')
-    print(spark.driver.memoryOverhead)
-    print(spark.executor.memoryOverhead)
     #Setting input Directory
     input_file = "../../../Tweets_Sorted/2020_3_31/"
     twitter = sqlCtx.read.json(input_file)
