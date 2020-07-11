f515ece23e43b9d6675f0ef11e72ede5c05ae3b1
akbog <alex.bogdanowicz@nyu.edu>
2020-07-10 20:02:54 -0400

    changes to spark

--- Spark/SparkJob.sh
+++ Spark/SparkJob.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 32
 #SBATCH -N 2
 #SBATCH -t 2-00:00
-#SBATCH --mem=64GB
+#SBATCH --mem=128GB
 #SBATCH --job-name spark-test
 #SBATCH --output spark-log-%J.txt
 
@@ -12,4 +12,4 @@ module load java/1.8.0_131
 module load anaconda3/5.2.0 spark
 source ../../../Scripts/venv-urban/bin/activate
 echo $JAVA_HOME
-spark-submit --total-executor-cores 64 --executor-memory 5G SparkJob.py 
+spark-submit --total-executor-cores 64 --executor-memory 10G SparkJob.py 
