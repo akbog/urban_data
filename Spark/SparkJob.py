ef5c81c5f7667cb6ef962ade647c0c9b3163edc5
akbog <alex.bogdanowicz@nyu.edu>
2020-07-10 20:29:19 -0400

    changes to spark

--- Spark/SparkJob.py
+++ Spark/SparkJob.py
@@ -9,23 +9,31 @@ from sparknlp import SparkSession
 from sparknlp.base import Finisher, DocumentAssembler
 from sparknlp.annotator import Tokenizer, Normalizer, LemmatizerModel, StopWordsCleaner, ViveknSentimentApproach, SentimentDetector, SentenceDetector, Stemmer, Lemmatizer
 from pyspark.ml import Pipeline
+import sys
+import time
 
 
 if __name__ == "__main__":
     #Launching Spark
-    spark = sparknlp.start()
-    # spark = SparkSession.builder \
-    #     .appName("Spark NLP")\
-    #     .master("local[*]")\
-    #     .config("spark.driver.memory","256G")\
-    #     .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.3")\
-    #     .config("spark.kryoserializer.buffer.max", "1000M")\
-    #     .getOrCreate()
-    sqlCtx = SQLContext(spark)
-    spark.sparkContext.setLogLevel('ERROR')
-    #Setting input Directory
-    input_file = "../../../Tweets_Sorted/2020_3_31/"
-    twitter = sqlCtx.read.json(input_file)
-    twitter.registerTempTable("tweets")
-    print("Reached Here")
-    print("Number of Tweets:", twitter.count())
+    # spark = sparknlp.start()
+    get_ip = sys.argv[1]
+    get_port = sys.argv[2]
+    spark = SparkSession.builder \
+        .appName("Spark NLP")\
+        .master("{}:{}".format(get_ip, get_port))\
+        .config("spark.driver.memory","200G")\
+        .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.3")\
+        .config("spark.kryoserializer.buffer.max", "1000M")\
+        .getOrCreate()
+    print("here")
+    while(True):
+        time.sleep(100)
+    print("here")
+    # sqlCtx = SQLContext(spark)
+    # spark.sparkContext.setLogLevel('ERROR')
+    # #Setting input Directory
+    # input_file = "../../../Tweets_Sorted/2020_3_31/"
+    # twitter = sqlCtx.read.json(input_file)
+    # twitter.registerTempTable("tweets")
+    # print("Reached Here")
+    # print("Number of Tweets:", twitter.count())
--- Spark/SparkJob.sh
+++ Spark/SparkJob.sh
@@ -12,4 +12,10 @@ module load java/1.8.0_131
 module load anaconda3/5.2.0 spark
 source ../../../Scripts/venv-urban/bin/activate
 echo $JAVA_HOME
-spark-submit --total-executor-cores 64 --executor-memory 200G SparkJob.py
+
+XDG_RUNTIME_DIR=""
+ipnport=$(shuf -i8000-9999 -n1)
+ipnip=$(hostname -i)
+echo -e "  ssh -N -L $ipnport:$ipnip:$ipnport $USER@hpc.shanghai.nyu.edu\n"
+echo -e "  localhost:$ipnport                                      \n\n"
+spark-submit --total-executor-cores 64 --executor-memory 200G SparkJob.py $ipnip $ipnport
