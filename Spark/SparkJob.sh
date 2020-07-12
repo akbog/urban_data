5d94119084a1b9cbfa987724353902b06a531c30
akbog <alex.bogdanowicz@nyu.edu>
2020-07-12 15:18:20 -0400

    spark

--- Spark/SparkJob.sh
+++ Spark/SparkJob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SBATCH -p parallel
-# SBATCH -n 16
-# SBATCH -N 1
+# SBATCH --cpus-per-task 5
+# SBATCH --nodes 3
 # SBATCH -t 2-00:00
 # SBATCH --mem=64GB
 # SBATCH --job-name spark-test
@@ -11,5 +11,10 @@ module purge
 module load java/1.8.0_131
 module load spark
 source ../../../Scripts/venv-urban/bin/activate
+
+
+ipnip=$(hostname -i)
+echo -e "  ssh -N -L 4040:$ipnip:4040 $USER@hpc.shanghai.nyu.edu\n"
+echo -e "  localhost:$ipnport                                      \n\n"
 
 spark-submit SparkJob.py
