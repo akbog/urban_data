356f31989955ebfea7d025eae229240af3ed060d
akbog <alex.bogdanowicz@nyu.edu>
2020-07-12 19:00:48 -0400

    spark

--- Spark/SparkJob.py
+++ Spark/SparkJob.py
@@ -15,15 +15,17 @@ import time
 
 if __name__ == "__main__":
     #Launching Spark
-    spark = sparknlp.start()
+    # spark = SparkSession.getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
+    # spark = sparknlp.start()
     print("Driver.MaxResultSize:", spark.sparkContext.getConf().get("spark.driver.maxResultSize"))
     print("Driver.Memory:", spark.sparkContext.getConf().get("spark.driver.memory"))
     print("Kryoserializer.Buffer.Max:", spark.sparkContext.getConf().get("spark.kryoserializer.buffer.max"))
-    sqlCtx = SQLContext(spark)
-    spark.sparkContext.setLogLevel('ERROR')
-    # Setting input Directory
-    input_file = "../../../Tweets_Sorted/2020_3_31/"
-    twitter = sqlCtx.read.json(input_file)
-    twitter.registerTempTable("tweets")
-    print("Reached Here")
-    print("Number of Tweets:", twitter.count())
+    # sqlCtx = SQLContext(spark)
+    # spark.sparkContext.setLogLevel('ERROR')
+    # # Setting input Directory
+    # input_file = "../../../Tweets_Sorted/2020_3_31/"
+    # twitter = sqlCtx.read.json(input_file)
+    # twitter.registerTempTable("tweets")
+    # print("Reached Here")
+    # print("Number of Tweets:", twitter.count())
