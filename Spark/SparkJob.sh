d80a55bb28ed6fee60d2ae02f07c4f0c7dab7e02
akbog <alex.bogdanowicz@nyu.edu>
2020-07-12 15:07:23 -0400

    this is an update

--- Spark/SparkJob.sh
+++ Spark/SparkJob.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-SBATCH -p parallel
-SBATCH -n 16
-SBATCH -N 1
-SBATCH -t 2-00:00
-SBATCH --mem=64GB
-SBATCH --job-name spark-test
-SBATCH --output spark-log.txt
+# SBATCH -p parallel
+# SBATCH -n 16
+# SBATCH -N 1
+# SBATCH -t 2-00:00
+# SBATCH --mem=64GB
+# SBATCH --job-name spark-test
+# SBATCH --output spark-log.txt
 
 module purge
 module load spark
