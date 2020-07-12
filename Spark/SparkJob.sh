4cb4e69448aff76224a67bd9ea69272a65777e33
akbog <alex.bogdanowicz@nyu.edu>
2020-07-12 15:04:10 -0400

    this is an update

--- Spark/SparkJob.sh
+++ Spark/SparkJob.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-SBATCH -p
+SBATCH -p parallel
 SBATCH -n 16
 SBATCH -N 1
 SBATCH -t 2-00:00
 SBATCH --mem=64GB
 SBATCH --job-name spark-test
-SBATCH --output spark-log-%J.txt
+SBATCH --output spark-log.txt
 
 module purge
-module load java/1.8.0_131
 module load spark
 
-spark-submit --total-executor-cores 64 --driver-memory G SparkJob.py
+spark-submit SparkJob.py
