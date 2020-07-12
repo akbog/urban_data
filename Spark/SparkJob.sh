08c788cbdeeea62520e69b23f18748c48c9463e7
akbog <alex.bogdanowicz@nyu.edu>
2020-07-12 15:09:34 -0400

    this is an update

--- Spark/SparkJob.sh
+++ Spark/SparkJob.sh
@@ -5,9 +5,11 @@
 # SBATCH -t 2-00:00
 # SBATCH --mem=64GB
 # SBATCH --job-name spark-test
-# SBATCH --output spark-log.txt
+# SBATCH --output spark-log-%J.txt
 
 module purge
+module load java/1.8.0_131
 module load spark
+source ../../../Scripts/venv-urban/bin/activate
 
 spark-submit SparkJob.py
