7ad2228b4d20b1d492409d65e581b9f0e8eb9c66
akbog <alex.bogdanowicz@nyu.edu>
2020-07-21 14:20:20 -0400

    newsparkjob

---
+++ Spark/job.spark.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+#SBATCH --nodes=2
+#SBATCH --output="slurm-%j.out"
+
+#SBATCH --time=01:30:00
+
+#SBATCH --job-name=sparktest
+
+#SBATCH --partition=serial
+
+#SBATCH --ntasks-per-node=16
+#SBATCH --exclusive
+#SBATCH --no-kill
+
+module load anaconda3/5.2.0 java/1.8.0_131 spark/2.4.6-hd2.7
+
+source ../../../Scripts/venv-urban/bin/activate
+
+module load ant/1.9.6
+
+export JAVA_HOME="/gpfsnyu/packages/java/jdk1.8.0_131"
+
+export SPARK_VERSION="2.4.6-hd2.7"
+
+export SPARK_HOME="/gpfsnyu/packages/spark/${SPARK_VERSION}"
+
+export SPARK_LOCAL_SCRATCH_DIR="/scratch/${USER}/sparkscratch/"
+
+export SPARK_LOCAL_DIR="/tmp/${USER}/spark"
+
+ipnip=$(hostname -i)
+echo -e "  ssh -N -L 4040:$ipnip:4040 $USER@hpc.shanghai.nyu.edu\n"
+
+XDG_RUNTIME_DIR=""
+ipnport=$(shuf -i8000-9999 -n1)
+ipnip=$(hostname -i)
+
+echo -e "\n"
+echo    "  Paste ssh command in a terminal on local host (i.e., laptop)"
+echo    "  ------------------------------------------------------------"
+echo -e "  ssh -N -L $ipnport:$ipnip:$ipnport $USER@hpc.shanghai.nyu.edu\n"
+echo    "  Open this address in a browser on local host; see token below"
+echo    "  ------------------------------------------------------------"
+echo -e "  localhost:$ipnport                                      \n\n"
+
+jupyter-lab --no-browser --port=$ipnport --ip=$ipnip
